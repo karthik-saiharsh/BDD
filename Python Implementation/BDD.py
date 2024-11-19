4305c8e2d97f8924a7cc8cca12025f54b8436720
Karthik Saiharsh <ks.the.visionary@gmail.com>
2024-11-19 20:10:16 +0530

    Added methods to simplify BDD

--- Python Implementation/BDD.py
+++ Python Implementation/BDD.py
@@ -40,6 +40,7 @@ class BinaryDecisionDiagram():
         self.zero = Node("0") # Terminal 0 Node
         self.arrayTree = [None] # Array represenation of the Binary Decision Tree
         self.variables = [] # Keep track of member variables
+        self.root = None
 
     def readCSV(self, path: str):
         # Read Truth table from csv
@@ -60,6 +61,7 @@ class BinaryDecisionDiagram():
         nodes = [None]
         for i in range(1, len(self.arrayTree)):
             if not self.arrayTree[i].isdigit():
+                # Add all non leaf nodes to the tree
                 new_node = Node(self.arrayTree[i])
                 if i != 1:
                     if i % 2 == 0:
@@ -68,6 +70,7 @@ class BinaryDecisionDiagram():
                         nodes[i//2].setRight(new_node)
                 nodes.append(new_node)
             else:
+                # Maintain only one set of zero and one nodes and reuse them
                 if i%2 == 0:
                     if self.arrayTree[i] == '1':
                         nodes[i//2].setLeft(self.one)
@@ -80,7 +83,56 @@ class BinaryDecisionDiagram():
                         nodes[i//2].setRight(self.zero)
         nodes += [self.one, self.zero]
         self.nodes = nodes
+        self.root = self.nodes[1]
+
+    def simplifyBDD(self):
+        '''Simplifies the BDD'''
+        self.__removeRedundant()
+        self.__removeDuplicates()
+
+    def __removeRedundant(self):
+        '''Removes redundant Nodes, i.e last but one level nodes with same left and right function valuation'''
+        nodes_to_delete = []
+        for i in range(1, len(self.nodes)):
+            if self.nodes[i].left != None and self.nodes[i].right != None:
+                if (self.nodes[i].left==self.nodes[i].right==self.one) or (self.nodes[i].left==self.nodes[i].right==self.zero):
+                    nodes_to_delete.append(self.nodes[i])
+        
+        for node in nodes_to_delete:
+            parent = node.parents[0]
+            child = node.left
+            child.parents.remove(node)
+            if parent.left == node:
+                parent.setLeft(child)
+            else:
+                parent.setRight(child)
+            self.nodes.remove(node)
+
+    def __removeDuplicates(self):
+        '''Remove all those nodes which are duplicates, i.e 2 different nodes with the same children'''
+        nodes_to_remove = []
+        for i in range(1, len(self.nodes)):
+            for j in range(1, len(self.nodes)):
+                if i != j and (self.nodes[i].left, self.nodes[i].right, self.nodes[j].left, self.nodes[j].right)!=(None, None, None, None):
+                    if (self.nodes[i].left, self.nodes[i].right) == (self.nodes[j].left, self.nodes[j].right):
+                        node_pair = {self.nodes[i], self.nodes[j]}
+                        if node_pair not in nodes_to_remove:
+                            nodes_to_remove.append(node_pair)
+        
+        # Remove the dulicate nodes
+        for node_pair in nodes_to_remove:
+            original, duplicate = node_pair
+            # Make the duplicate node disown it's children
+            duplicate.left.parents.remove(duplicate)
+            duplicate.right.parents.remove(duplicate)
+            if duplicate.parents[0] == duplicate:
+                duplicate.parents[0].setLeft(original)
+            else:
+                duplicate.parents[0].setRight(original)
+            self.nodes.remove(duplicate)
 
 BDD = BinaryDecisionDiagram()
-BDD.readCSV("Python Implementation/tt.csv")
+BDD.readCSV("Python Implementation/tt2.csv")
 BDD.buildBDD()
+BDD.simplifyBDD()
+print()
